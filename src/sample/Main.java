2bda704b79c23dabaac3f3c78878fd0695a84a4a
borzah <borzah@gmail.com>
2019-07-20 16:06:45 +0300

    Interface

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,9 +15,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("map.fxml"));
-        Parent root =  loader.load();
+        Pane root =  loader.load();
         primaryStage.setTitle("PathFinder");
         primaryStage.setScene(new Scene(root, 300, 275));
+        Button exitBtn = new Button("Exit X");
+        exitBtn.setOnAction(e -> Platform.exit());
+        root.getChildren().add(exitBtn);
         primaryStage.show();
     }
 
