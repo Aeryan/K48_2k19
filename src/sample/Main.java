48c4c08dcc30646e72fd67e228711164d51bad3c
borzah <borzah@gmail.com>
2019-07-20 17:38:50 +0300

    Interface, ver 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,12 +17,22 @@ import java.security.Key;
 
 public class Main extends Application {
 
+    public String pictureName = "somemap.png";
+
+    private String greenStyle = "-fx-background-color:\n" +
+            "        linear-gradient(#f0ff35, #a9ff00),\n" +
+            "                radial-gradient(center 50% -40%, radius 200%, #b8ee36 45%, #80c800 50%);\n" +
+            "        -fx-background-radius: 6, 5;\n" +
+            "        -fx-background-insets: 0, 1;\n" +
+            "        -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.4) , 5, 0.0 , 0 , 1 );\n" +
+            "        -fx-text-fill: #395306;";
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("map.fxml"));
         Pane root =  loader.load();
         primaryStage.setTitle("PathFinder");
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 500, 300);
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent e) {
@@ -33,8 +43,13 @@ public class Main extends Application {
         });
         primaryStage.setScene(scene);
         Button exitBtn = new Button("Exit X");
+        exitBtn.setStyle(greenStyle);
         exitBtn.setOnAction(e -> Platform.exit());
+        Button actionButton = new Button("Path");
+        actionButton.setLayoutX(60);
+        actionButton.setStyle(greenStyle);
         root.getChildren().add(exitBtn);
+        root.getChildren().add(actionButton);
         primaryStage.show();
     }
 
