3487e91f8ac3dee77c3bdbc4ddea89e4ef070251
borzah <borzah@gmail.com>
2019-07-20 22:08:39 +0300

    Interface

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -17,10 +18,12 @@ public class Main extends Application {
 
     private static final int SCENEWIDTHX = 600;
     private static final int SCENEHEIGHTX = 300;
-    private static final int ACTIONBUTTONLAYOUTX = 60;
+    private static final int ACTIONBUTTONLAYOUTX = 57;
     private static final int INFOBUTTONLAYOUTX = 120;
     private static final int INZOOMLABELLAYOUTX = 180;
     private static final int OUTZOOMLAYOUTX = 260;
+    private static final int BACKBUTTONLAYOUTX = 0;
+    private static final int BACKBUTTONLLAYOUTY = 0;
 
     private String greenStyle = "-fx-background-color:\n" +
             "        linear-gradient(#f0ff35, #a9ff00),\n" +
@@ -33,14 +36,39 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("map.fxml"));
+        FXMLLoader secondLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
         Pane root =  loader.load();
+        Pane secondRoot = secondLoader.load();
         primaryStage.setTitle("PathFinder");
+        //Scene without path
         Scene scene = new Scene(root, SCENEWIDTHX, SCENEHEIGHTX);
+        //Scene with path
+        Scene scene1 = new Scene(secondRoot, SCENEWIDTHX, SCENEHEIGHTX);
+        //Second scene back button
+        Button backButton = new Button("Back E");
+        //Getting first scene back
+        backButton.setOnAction(e -> primaryStage.setScene(scene));
+        backButton.setStyle(greenStyle);
+        backButton.setLayoutY(BACKBUTTONLLAYOUTY);
+        backButton.setLayoutX(BACKBUTTONLAYOUTX);
+        secondRoot.getChildren().add(backButton);
+        // First scene settings
+        scene1.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.E) {
+                    primaryStage.setScene(scene);
+                }
+            }
+        });
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent e) {
                 if(e.getCode() == KeyCode.X) {
                     Platform.exit();
+                }
+                if (e.getCode() == KeyCode.P) {
+                    primaryStage.setScene(scene1);
                 }
             }
         });
@@ -48,9 +76,11 @@ public class Main extends Application {
         Button exitBtn = new Button("Exit X");
         exitBtn.setStyle(greenStyle);
         exitBtn.setOnAction(e -> Platform.exit());
-        Button actionButton = new Button("Path");
+        Button actionButton = new Button("Path P");
         actionButton.setLayoutX(ACTIONBUTTONLAYOUTX);
         actionButton.setStyle(greenStyle);
+        //Getting second scene
+        actionButton.setOnAction(e -> primaryStage.setScene(scene1));
         Button infoButton = new Button("Info");
         infoButton.setLayoutX(INFOBUTTONLAYOUTX);
         infoButton.setStyle(greenStyle);
@@ -67,7 +97,6 @@ public class Main extends Application {
         root.getChildren().add(infoButton);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
