b398eeb9d034ddc0a53b2e5990abcbf76f49e13d
borzah <borzah@gmail.com>
2019-07-20 16:34:43 +0300

    Interface, ver 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button zoomInButton;
+
+    @FXML
+    private Button zoomOutButton;
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,13 +2,18 @@ package sample;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.security.Key;
 
 public class Main extends Application {
 
@@ -17,7 +22,16 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("map.fxml"));
         Pane root =  loader.load();
         primaryStage.setTitle("PathFinder");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 300, 275);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                if(e.getCode() == KeyCode.X) {
+                    Platform.exit();
+                }
+            }
+        });
+        primaryStage.setScene(scene);
         Button exitBtn = new Button("Exit X");
         exitBtn.setOnAction(e -> Platform.exit());
         root.getChildren().add(exitBtn);
